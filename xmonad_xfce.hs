86edad87a88fbc788705c7ec67f618ddb31f3f47
Max Jonas Werner <mail@makk.es>
2015-03-10 10:25:27 +0100

    Xmonad: added configuration for Xfce

--- xmonad_xfce.hs
+++ xmonad_xfce.hs
@@ -13,6 +13,7 @@ import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Minimize
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.PerWorkspace
 import XMonad.Util.EZConfig
 import qualified XMonad.StackSet as W
 
@@ -20,33 +21,38 @@ myModMask = mod1Mask
 
 myManageHook = composeAll
     [ manageHook xfceConfig
-    , className =? "Xfrun4" --> doCenterFloat
-    , className =? "Screenruler" --> doFloat
-    , className =? "Firefox" --> doF (W.shift "web")
+    , resource  =? "synapse"          --> doIgnore
+    , className =? "Xfrun4"           --> doCenterFloat
+    , className =? "Kruler"      --> doFloat
+    , className =? "Firefox"          --> doF (W.shift "web")
+    , (className =? "Firefox" <&&> resource =? "Browser") --> doFloat
+    , (className =? "Firefox" <&&> title =? "Inspect Network Request") --> (ask >>= doF . W.sink)
     , className =? "Chromium-browser" --> doF (W.shift "web")
-    , className =? "Pidgin" --> doF (W.shift "chat")
-    , className =? "Skype" --> doF (W.shift "chat")
-    , className =? "Thunderbird" --> doF (W.shift "mail")
-    , className =? "Rhythmbox" --> doF (W.shift "media")
-    , className =? "Spotify" --> doF (W.shift "media")
-    , className =? "Eclipse" --> doF (W.shift "dev")
+    , className =? "Pidgin"           --> doF (W.shift "chat")
+    , className =? "Skype"            --> doF (W.shift "chat")
+    , className =? "Thunderbird"      --> doF (W.shift "mail")
+    , className =? "Rhythmbox"        --> doF (W.shift "media")
+    , className =? "Spotify"          --> doF (W.shift "media")
+    , className =? "Eclipse"          --> doF (W.shift "dev")
     ]
     <+>
     composeOne [ isFullscreen -?> doFullFloat ]
 
 defaultLayouts = smartBorders( desktopLayoutModifiers (Tall 1 (3/100) (1/2) ||| noBorders Full) )
+flexibleLayout = smartBorders $ desktopLayoutModifiers(Grid)
 
 myLayouts = minimize $ defaultLayouts
 
 main = xmonad $ xfceConfig { 
     focusFollowsMouse = True,
     manageHook = myManageHook <+> fullscreenManageHook,
-    layoutHook = myLayouts,
+    layoutHook = onWorkspace "offshore" flexibleLayout $ myLayouts,
     handleEventHook = handleEventHook xfceConfig <+> fullscreenEventHook <+> minimizeEventHook,
     workspaces = ["web", "chat", "mail", "dev", "offshore", "media"]
     } 
     `additionalKeys`
         [ ((myModMask, xK_m), withFocused minimizeWindow)
+        , ((myModMask, xK_p), spawn "synapse") 
         , ((myModMask .|. shiftMask, xK_m), sendMessage RestoreNextMinimizedWin) 
         , ((myModMask, xK_semicolon), prevWS)
         , ((myModMask, xK_apostrophe), nextWS)
